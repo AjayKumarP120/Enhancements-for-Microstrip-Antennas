6e417402568bf6ba2581a716a0d8cd6c2812e483
AjayKumarP120 <159688706+AjayKumarP120@users.noreply.github.com>
2024-02-11 14:58:25 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,28 @@
+# Antenna Generator
+A python library that allows you to generate microstrip patch antenna in the HFSS  software
+
+
+## Installation
+
+```shell
+git clone https://github.com/JoyenBenitto/Antenna_generator.git
+cd Antenna_generator
+pip install -e .
+```
+
+## Usage
+
+To display the help message, run:
+```shell
+ant_gen --help
+ant_gen generate --help
+```
+
+To generate an antenna, run:
+```shell
+ant_gen generate --src [SOURCE]
+```
+To generate an antenna into a build directory:
+```shell
+ant_gen generate --build_dir [dir] 
+```
---
+++ setup.py
@@ -0,0 +1,50 @@
+"""The setup script."""
+
+import os
+from setuptools import setup, find_packages
+import codecs
+from ant_gen.__init__ import __version__
+from ant_gen.__init__ import __author__
+from ant_gen.__init__ import __mail__
+
+# Base directory of package
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+def read_requires():
+    with open(os.path.join(here, "ant_gen/requirements.txt"),
+              "r") as reqfile:
+        return reqfile.read().splitlines()
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+
+setup(
+    name='ant_gen',
+    version=__version__,
+    description="Antenna Generator",
+    long_description= readme + '\n\n',
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: MIT",
+        "Development Status :: Beta"
+    ],
+    keywords = "ant_gen",
+    url = 'https://github.com/JoyenBenitto/Antenna_generator',
+    author = __author__,
+    author_email = __mail__,
+    license = "MIT License",
+    packages = find_packages(),
+    install_requires = ["requests"],
+    python_requires = ">=3.8",
+    entry_points={
+        'console_scripts': ['ant_gen=ant_gen.main:cli'],
+    },
+    include_package_data=True,
+    tests_require=[],
+    zip_safe=False
+)
